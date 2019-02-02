f74a7abe31f0736464b676e2044c29076d4424f8
gt6089 <kealyd@gmail.com>
2019-02-01 23:18:02 -0800

    add bootstrap

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,6 +4,13 @@ class Player < ApplicationRecord
 
   has_secure_password
 
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+
   def full_name
     "#{first_name} #{last_name}"
   end
