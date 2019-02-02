50774cd34f24fc3286c327b4383840973a97be4b
gt6089 <kealyd@gmail.com>
2019-02-02 01:26:21 -0800

    add sessions and cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 
     if player && player.authenticate(params[:session][:password])
       log_in player
+      remember player
       redirect_to player
     else
       flash.now[:danger] = 'Invalid email or password'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-      log_out
+      log_out if logged_in?
       redirect_to root_path
     end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,16 +3,35 @@ module SessionsHelper
     session[:player_id] = player.id
   end
 
+  def remember(player)
+    player.remember
+    cookies.permanent.signed[:player_id] = player.id
+    cookies.permanent[:remember_token] = player.remember_token
+  end
+
   def current_user
-    return nil unless session[:player_id]
-    @current_user ||= Player.find_by(id: session[:player_id])
+    if (player_id = session[:player_id])
+      @current_user ||= Player.find_by(id: player_id)
+    elsif (player_id = cookies.signed[:player_id])
+      player = Player.find_by(id: player_id)
+      if player && player.authenticated?(cookies[:remember_token])
+        log_in player
+        @current_user = player
+    end
   end
 
   def logged_in?
     !current_user.nil?
   end
 
+  def forget(player)
+    player.forget
+    cookies.delete(:player_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:player_id)
     @current_user = nil
   end
